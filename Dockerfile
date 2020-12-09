31e9db79176f6a830deef86e36dbe48c5826e0ab
cbryant24 <chrs.bryant@gmail.com>
2020-12-09 00:28:20 -0800

    node worker and ip-api working with nginx proxy

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,8 @@ COPY nginx.conf /etc/nginx/nginx.conf
 
 COPY default.conf /etc/nginx/conf.d/default.conf
 
+COPY worker.conf /etc/nginx/conf.d/worker.conf
+
 EXPOSE 80
+
+EXPOSE 4000
--- worker/dist/index.js
+++ worker/dist/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 app.use(express.static('public'));
 app.listen(port, () => console.log(`App listening on port ${port}`));
 //# sourceMappingURL=index.js.map
